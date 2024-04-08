c5b35e200a55c606dd6f0a97c1af9c008807af41
Ankur Kumar <bitcoinwallet02525@gmail.com>
2024-04-08 10:24:21 +0530

    initial commit

---
+++ core/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+from .views import (
+    ItemDetailView,
+    HomeView,
+    add_to_cart,
+    remove_from_cart,
+    ShopView,
+    OrderSummaryView,
+    remove_single_item_from_cart,
+    CheckoutView,
+    PaymentView,
+    AddCouponView,
+    RequestRefundView,
+    CategoryView,
+    stripe_config,
+    create_checkout_session,
+    MyOrders
+)
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('category/<slug>/', CategoryView.as_view(), name='category'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add_coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund'),
+    path('config/', stripe_config),  # new
+    path('create-checkout-session/', create_checkout_session), # new
+    path('my-orders/', MyOrders.as_view(), name='my-orders'), # new
+
+]
